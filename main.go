f178ac53b4a139c6f99c8ca65a5af0dd2a5e1f00
bkm20 <bkm20@ic.ac.uk>
2022-12-01 15:18:48 +0000

    Handle required inside init() function

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	spin "github.com/fermyon/spin/sdk/go/http"
 )
 
-func main() {
+func init() {
 	spin.Handle(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, World!")
 	})
+}
+
+func main() {
 }
