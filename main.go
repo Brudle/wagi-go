cfac857a1420974c4686154f92085c443d13db8c
bkm20 <bkm20@ic.ac.uk>
2022-12-02 13:16:09 +0000

    deployed with wagi

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	spin "github.com/fermyon/spin/sdk/go/http"
 )
 
-func init() {
-	spin.Handle(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-        fmt.Fprintf(w, "\n")
-		fmt.Fprintln(w, "Hello, World!")
-	})
-}
-
 func main() {
+	fmt.Println("Content-Type: text/plain; charset=utf-8")
+	fmt.Println()
+	fmt.Println("Hello World!")
 }
